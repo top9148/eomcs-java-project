30e44bc57180ed2593c74470105bf72928e0098f
Jinyoung Eom <jinyoung.eom@gmail.com>
2019-01-01 10:44:08 +0900

    .

--- eomcs-java-project-16/src/main/java/com/eomcs/lms/handler/MemberList.java
+++ eomcs-java-project-16/src/main/java/com/eomcs/lms/handler/MemberList.java
@@ -4,32 +4,32 @@ import java.util.Arrays;
 import com.eomcs.lms.domain.Member;
 
 public class MemberList {
-  final int DEFAULT_CAPACITY = 10;
+  static final int DEFAULT_CAPACITY = 10;
   Member[] members;
   int size = 0;
-  
+
   public MemberList() {
     members  = new Member[DEFAULT_CAPACITY];
   }
-  
+
   public MemberList(int initialCapacity) {
-    if (initialCapacity > DEFAULT_CAPACITY) 
+    if (initialCapacity > DEFAULT_CAPACITY)
       members = new Member[initialCapacity];
     else
       members = new Member[DEFAULT_CAPACITY];
   }
-  
+
   public Member[] toArray() {
-    return Arrays.copyOf(members, size); 
+    return Arrays.copyOf(members, size);
   }
-  
+
   public void add(Member member) {
     if (size >= members.length) {
       int oldCapacity = members.length;
       int newCapacity = oldCapacity + (oldCapacity >> 1);
       members = Arrays.copyOf(members, newCapacity);
     }
-    
+
     members[size++] = member;
   }
 }
--- eomcs-java-project-18/src/main/java/com/eomcs/util/ArrayList.java
+++ eomcs-java-project-18/src/main/java/com/eomcs/util/ArrayList.java
@@ -3,21 +3,21 @@ package com.eomcs.util;
 import java.util.Arrays;
 
 public class ArrayList<T> {
-  final int DEFAULT_CAPACITY = 10;
+  static final int DEFAULT_CAPACITY = 10;
   Object[] elementData;
   int size = 0;
-  
+
   public ArrayList() {
     elementData  = new Object[DEFAULT_CAPACITY];
   }
-  
+
   public ArrayList(int initialCapacity) {
-    if (initialCapacity > DEFAULT_CAPACITY) 
+    if (initialCapacity > DEFAULT_CAPACITY)
       elementData = new Object[initialCapacity];
     else
       elementData = new Object[DEFAULT_CAPACITY];
   }
-  
+
   @SuppressWarnings("unchecked")
   public T[] toArray(T[] a) {
     if (a.length < size) {
@@ -28,14 +28,14 @@ public class ArrayList<T> {
       a[size] = null;
     return a;
   }
-  
+
   public void add(T obj) {
     if (size >= elementData.length) {
       int oldCapacity = elementData.length;
       int newCapacity = oldCapacity + (oldCapacity >> 1);
       elementData = Arrays.copyOf(elementData, newCapacity);
     }
-    
+
     elementData[size++] = obj;
   }
 }
